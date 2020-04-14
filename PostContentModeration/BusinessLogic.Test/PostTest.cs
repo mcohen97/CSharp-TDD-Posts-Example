0e2f838838255d5aaf1c18d08b20514d92c07065
Marcel Cohen <diegomarcel27@hotmail.com>
2020-04-13 23:29:55 -0300

    Added test class for Post.

---
+++ PostContentModeration/BusinessLogic.Test/PostTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class PostTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
