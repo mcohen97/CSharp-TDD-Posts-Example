7bec3655761473a98061d2485cfd106181c729fc
Marcel Cohen <diegomarcel27@hotmail.com>
2020-04-13 23:43:25 -0300

    Created Post class with the minimum implementation to make the tests pass.

---
+++ PostContentModeration/BusinessLogic/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Post
+    {
+        public string Title { get; private set; }
+        public string Body { get; private set; }
+        public DateTime DatePublished { get; private set; }
+
+        public Post(string aTitle, string aBody, DateTime aDate) {
+            Title = aTitle;
+            Body = aBody;
+            DatePublished = aDate;
+        }
+    }
+}
