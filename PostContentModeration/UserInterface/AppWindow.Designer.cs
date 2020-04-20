7c7bbba0f6ce05d513157d0524a19297c00b0a78
Marcel Cohen <diegomarcel27@hotmail.com>
2020-04-20 17:09:23 -0300

    Added menu with option buttons and panel where function will be displayed.

--- PostContentModeration/UserInterface/AppWindow.Designer.cs
+++ PostContentModeration/UserInterface/AppWindow.Designer.cs
@@ -28,20 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.optionPanel = new System.Windows.Forms.Panel();
+            this.buttonsPanel = new System.Windows.Forms.Panel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnViewPosts = new System.Windows.Forms.Button();
+            this.buttonsPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // optionPanel
+            // 
+            this.optionPanel.Location = new System.Drawing.Point(-2, 82);
+            this.optionPanel.Name = "optionPanel";
+            this.optionPanel.Size = new System.Drawing.Size(802, 370);
+            this.optionPanel.TabIndex = 0;
+            // 
+            // buttonsPanel
+            // 
+            this.buttonsPanel.Controls.Add(this.btnViewPosts);
+            this.buttonsPanel.Controls.Add(this.btnAdd);
+            this.buttonsPanel.Location = new System.Drawing.Point(-2, 0);
+            this.buttonsPanel.Name = "buttonsPanel";
+            this.buttonsPanel.Size = new System.Drawing.Size(801, 84);
+            this.buttonsPanel.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(14, 21);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(131, 46);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Create Post";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnViewPosts
+            // 
+            this.btnViewPosts.Location = new System.Drawing.Point(163, 21);
+            this.btnViewPosts.Name = "btnViewPosts";
+            this.btnViewPosts.Size = new System.Drawing.Size(129, 46);
+            this.btnViewPosts.TabIndex = 1;
+            this.btnViewPosts.Text = "View Posts";
+            this.btnViewPosts.UseVisualStyleBackColor = true;
             // 
             // AppWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonsPanel);
+            this.Controls.Add(this.optionPanel);
             this.Name = "AppWindow";
             this.Text = "Posts Example";
+            this.buttonsPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel optionPanel;
+        private System.Windows.Forms.Panel buttonsPanel;
+        private System.Windows.Forms.Button btnViewPosts;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
