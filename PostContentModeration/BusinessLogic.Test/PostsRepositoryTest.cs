664b0f04f5d84bfc6f46de0967eb4d58679da6ad
Marcel Cohen <diegomarcel27@hotmail.com>
2020-04-20 13:42:14 -0300

    Added test class for a posts repository.

---
+++ PostContentModeration/BusinessLogic.Test/PostsRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class PostsRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
