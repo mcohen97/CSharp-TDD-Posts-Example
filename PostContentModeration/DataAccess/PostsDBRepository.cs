6f22e9da05c9b2ef443107592f274a825cc50023
Marcel Cohen <diegomarcel27@hotmail.com>
2020-05-13 15:32:27 -0300

    Implemented posts database repository functions.

--- PostContentModeration/DataAccess/PostsDBRepository.cs
+++ PostContentModeration/DataAccess/PostsDBRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,69 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    class PostsDBRepository
+    public class PostsDBRepository : IRepository<Post>
     {
+        public void Add(Post post)
+        {
+            using (PostsContext context = new PostsContext()) {
+                context.Posts.Add(post);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (PostsContext context = new PostsContext())
+            {
+                Post toRemove = context.Posts.First(p => p.Id == id);
+
+                if (toRemove == null)
+                {
+                    throw new PostNotFoundExeption();
+                }
+
+                context.Posts.Remove(toRemove);
+                context.SaveChanges();
+            }
+        }
+
+        public bool Exists(int id)
+        {
+            using (PostsContext context = new PostsContext())
+            {
+                return context.Posts.Any(p => p.Id == id);
+            }
+        }
+
+        public Post Get(int id)
+        {
+            using (PostsContext context = new PostsContext())
+            {
+                Post found = context.Posts.First(p => p.Id == id);
+
+                if (found == null)
+                {
+                    throw new PostNotFoundExeption();
+                }
+
+                return found;
+            }
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            using (PostsContext context = new PostsContext())
+            {
+                return context.Posts;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            using (PostsContext context = new PostsContext())
+            {
+                return !context.Posts.Any();
+            }
+        }
     }
 }
