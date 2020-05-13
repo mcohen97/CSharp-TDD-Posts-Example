dcf6d358c32f07b5a89bfb8bae77d73edb4fca3b
Marcel Cohen <diegomarcel27@hotmail.com>
2020-05-13 16:21:20 -0300

    Added posts entity.

---
+++ PostContentModeration/DataAccess/PostEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class PostEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime PublishedDate { get; set; }
+
+    }
+}
