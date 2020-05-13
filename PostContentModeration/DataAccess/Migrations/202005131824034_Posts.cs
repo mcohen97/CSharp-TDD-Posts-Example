f6c19ef5446e0eab67c1e600c8aa17a398031ff6
Marcel Cohen <diegomarcel27@hotmail.com>
2020-05-13 15:32:41 -0300

    Applied migrations to database.

---
+++ PostContentModeration/DataAccess/Migrations/202005131824034_Posts.cs
@@ -0,0 +1,28 @@
+﻿namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Posts : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Body = c.String(),
+                        DatePublished = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
