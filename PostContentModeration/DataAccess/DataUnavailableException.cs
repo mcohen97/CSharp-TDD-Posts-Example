e8071ce5923f0b7dabb71918c4087b347f61aaeb
Marcel Cohen <diegomarcel27@hotmail.com>
2020-06-24 19:25:10 -0300

    Creaded exception for case when database can't be accesed.

---
+++ PostContentModeration/DataAccess/DataUnavailableException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataAccess
+{
+    public class DataUnavailableException : Exception
+    {
+        public DataUnavailableException() : base("Can't access data source")
+        {
+        }
+    }
+}
